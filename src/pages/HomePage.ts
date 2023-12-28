f44edcd31b0368b22855b7681604f1b747861631
mukunthan <mukunthan.r2@gmail.com>
2023-12-28 08:49:53 +0530

    refactoring

--- src/pages/HomePage.ts
+++ src/pages/HomePage.ts
@@ -2,17 +2,14 @@ import { Page, expect } from "@playwright/test";
 import logger from "../utils/LoggerUtil";
 
 export default class HomePage {
-  
   private readonly serviceTitleLocator = "Service";
 
-
-  constructor(private page: Page) {
-    
-  }
-
+  constructor(private page: Page) {}
 
   async expectServiceTitleToBeVisible() {
-    await expect(this.page.getByTitle(this.serviceTitleLocator)).toBeVisible({ timeout: 15000 });
-    logger.info("Service Title is visible")
+    await expect(this.page.getByTitle(this.serviceTitleLocator)).toBeVisible({
+      timeout: 15000,
+    });
+    logger.info("Service Title is visible");
   }
 }
