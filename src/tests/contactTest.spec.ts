f590a0a03569ac61c745e7478ea6e8934445321c
mukunthan <mukunthan.r2@gmail.com>
2024-01-02 10:22:39 +0530

    faker data implementation

--- src/tests/contactTest.spec.ts
+++ src/tests/contactTest.spec.ts
@@ -2,11 +2,12 @@ import { test } from "@playwright/test";
 import LoginPage from "../pages/loginPage";
 import { decrypt } from "../utils/CryptojsUtil";
 import logger from "../utils/LoggerUtil";
-import cdata from "../testdata/dataop.json";
+import cdata from "../testdata/datademo.json";
 import { convertCsvFileToJsonFile } from "../utils/CSVtoJSONUtil";
+import { exportToCsv, exportToJson, generateTestData } from "../utils/FakerDataUtil";
 
 for (const contact of cdata) {
-  test(`Advance DD test for ${contact.firstName} `, async ({ page }) => {
+  test.skip(`Advance DD test for ${contact.firstName} `, async ({ page }) => {
     logger.info("Test for Contact Creation is started...");
     const loginPage = new LoginPage(page);
     await loginPage.navigateToLoginPage();
@@ -26,8 +27,8 @@ for (const contact of cdata) {
 
 test.skip("simple DD test", async ({ page }) => {
   logger.info("Test for Contact Creation is started...");
-  const fname = "Abirami";
-  const lname = "M";
+  const fname = "Shiva";
+  const lname = "Rudra";
   const loginPage = new LoginPage(page);
   await loginPage.navigateToLoginPage();
   await loginPage.fillUsername(decrypt(process.env.userid!));
@@ -44,5 +45,18 @@ test.skip("simple DD test", async ({ page }) => {
 });
 
 test.skip("csv to json", async () => {
-  convertCsvFileToJsonFile("data.csv", "dataop.json");
+  convertCsvFileToJsonFile("data.csv", "datademo.json");
 });
+
+test("Faker", async ({ page }) => { 
+
+  // Generate test data
+const testData = generateTestData(20);
+
+// Export data to JSON file
+exportToJson(testData, 'testData_en.json');
+
+// Export data to CSV file
+exportToCsv(testData, 'testData_en.csv');
+
+ });
---
+++ src/utils/FakerDataUtil.ts
@@ -0,0 +1,78 @@
+import { faker } from '@faker-js/faker';
+import * as fs from 'fs';
+import * as createCsvWriter from 'csv-writer';
+import path from 'path';
+
+
+// Define the type for the user data
+interface UserData {
+  name: string;
+  email: string;
+  username: string;
+  phone: string;
+  age: number;
+  address: string;
+}
+
+// Function to generate fake user data
+const generateUserData = (): UserData => {
+  return {
+    name: faker.person.firstName(),
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    phone: faker.phone.number(),
+    age: faker.number.int({ min: 18, max: 99 }),
+    address: faker.location.country(),
+  };
+};
+
+// Function to generate an array of fake user data
+export const generateTestData = (numRecords: number): UserData[] => {
+  const testData: UserData[] = faker.helpers.multiple(generateUserData, {
+  count: numRecords});
+  return testData;
+};
+
+const currentDir = __dirname;
+// Go one level above (back to 'src')
+const srcDir = path.resolve(currentDir, "..");
+
+// Change to 'config' folder
+const testdataDir = path.resolve(srcDir, "testdata");
+
+// Function to export data to JSON file
+export const exportToJson = (data: UserData[], fileName: string) => {
+  fs.writeFileSync(`${testdataDir}\\${fileName}`, JSON.stringify(data, null, 2));
+  console.log(`Data exported to JSON file: ${testdataDir}\\${fileName}`);
+};
+
+// Function to export data to CSV file
+export const exportToCsv = (data: UserData[], fileName: string) => {
+
+  const csvWriter = createCsvWriter.createObjectCsvWriter({
+    path: `${testdataDir}\\${fileName}`,
+    header: [
+      { id: 'name', title: 'Name' },
+      { id: 'email', title: 'Email' },
+      { id: 'username', title: 'Username' },
+      { id: 'password', title: 'Password' },
+      { id: 'age', title: 'Age' },
+      { id: 'address', title: 'Address' },
+    ],
+  });
+
+ 
+
+  csvWriter.writeRecords(data).then(() => {
+    console.log(`Data exported to CSV file: ${testdataDir}\\${fileName}`);
+  });
+};
+
+// Generate test data
+// const testData = generateTestData(3);
+
+// // Export data to JSON file
+// exportToJson(testData, 'testData_en.json');
+
+// // Export data to CSV file
+// exportToCsv(testData, 'testData_en.csv');
