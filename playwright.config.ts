1a50d37edfd59cff0563b923419ee6e032c17953
mukunthan <mukunthan.r2@gmail.com>
2024-01-23 21:59:01 +0100

    video on

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,6 +41,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     screenshot: "on",
+    video: "on"
   },
 
   /* Configure projects for major browsers */
