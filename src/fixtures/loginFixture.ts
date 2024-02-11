1eeae77eab101f46b2b3eda75065a4023c340c76
mukunthan <mukunthan.r2@gmail.com>
2024-02-11 21:36:26 +0100

    custom fixtures set up

---
+++ src/fixtures/loginFixture.ts
@@ -0,0 +1,22 @@
+import { Page, test as base, expect as defaultExpect } from "@playwright/test";
+import LoginPage from "../pages/LoginPage";
+import HomePage from "../pages/HomePage";
+import { decrypt } from "../utils/CryptojsUtil";
+
+type UIPages = {
+  homePage: HomePage;
+  page: Page;
+};
+
+export const expect = defaultExpect;
+// Define a custom fixture with page
+export const test = base.extend<UIPages>({
+  homePage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigateToLoginPage();
+    await loginPage.fillUsername(decrypt(process.env.userid!));
+    await loginPage.fillPassword(decrypt(process.env.password!));
+    const homePage = await loginPage.clickLoginButton();
+    await use(homePage);
+  },
+});
---
+++ src/tests/fixtureTest.spec.ts
@@ -0,0 +1,6 @@
+import { test } from "../fixtures/loginFixture";
+
+test("Fixture test", async ({ homePage }) => {
+  await homePage.expectServiceTitleToBeVisible();
+
+});
